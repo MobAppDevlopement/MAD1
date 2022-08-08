865253fdbfbb21af882c1888bb50ec497f379bc3
tejasvi <pateltejasvi329@gmail.com>
2022-08-08 08:18:20 +0530

    P1_1

---
+++ src/main/kotlin/practical-1.kt
@@ -0,0 +1,22 @@
+fun main() {
+    var x : Int=100
+    var y : Float=21.43F
+    var a : Char='S'
+    val str="Saumil"
+    var bool : Boolean=true
+    var b : Double=34.34565
+    var c : Long=3000000000000
+    var d : Short=-3456
+    var e : Byte=-26
+
+    println("Integer Value : $x")
+    println("Float Value : $y")
+    println("Character Value : $a")
+    println("String Value : $str")
+    println("Boolean Value : $bool")
+    println("Double Value : $b")
+    println("Long Value : $c")
+    println("Short Value : $d")
+    println("Byte Value : $e")
+
+}
